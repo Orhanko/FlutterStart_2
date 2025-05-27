996337519e871a23cc6c56a3a70ee43d48fab202
Orhan Pojskic <ohran.pojskic.20@size.ba>
2025-05-27 17:09:45 +0200

    Created real questions and their answers

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -1,10 +1,39 @@
 import 'package:pocetak2/models/quiz_question.dart';
 
 const questions = [
-  QuizQuestion('First Question', ['1', '2', '3', '4']),
-  QuizQuestion('Second Question', ['5', '6', '7', '8']),
-  QuizQuestion('Third Question', ['9', '10', '11', '12']),
-  QuizQuestion('Fourth Question', ['13', '14', '15', '16']),
-  QuizQuestion('Fifth Question', ['17', '18', '19', '20']),
-  QuizQuestion('Sixth Question', ['21', '22', '23', '24']),
+  QuizQuestion(
+    'Which technology does Orhan primarily use for developing iOS applications?',
+    ['SwiftUI', 'React Native', 'Kotlin', 'Xamarin'],
+  ),
+  QuizQuestion('Which device does Orhan mainly use for app development?', [
+    "MacBook Air",
+    "iPhone 15",
+    "Windows Desktop",
+    "iPad Pro",
+  ]),
+  QuizQuestion('At which faculty did Orhan complete his degree?', [
+    "Polytechnic Faculty Zenica",
+    "Faculty of Electrical Engineering Sarajevo",
+    "Technical Faculty Tuzla",
+    "Faculty of Information Technologies Mostar",
+  ]),
+  QuizQuestion('What is the title of Orhan’s recently defended thesis?', [
+    "Interactive Visualization of Statistical Data Through an iOS Application",
+    "Data Visualization Through Android Application",
+    "Interactive Market Analysis in Real Time",
+    "Statistical Data Processing in Java",
+  ]),
+  QuizQuestion(
+    'What was Orhan’s main goal in transitioning from iOS to Flutter?',
+    [
+      "Learning cross-platform development",
+      "Faster app performance",
+      "Using Dart because of its simplicity",
+      "Better support for macOS",
+    ],
+  ),
+  QuizQuestion(
+    'What is the name of the restaurant Orhan planned to visit after successfully defending his thesis?',
+    ["Oliva", "Urban Grill", "Taboo", "Palazzo"],
+  ),
 ];
